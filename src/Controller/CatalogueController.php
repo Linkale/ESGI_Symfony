a844ef6386a0090aa221d7795afac1f1fb4b1eca
Pierre Landry <45065999+Linkale@users.noreply.github.com>
2022-05-04 16:11:36 +0200

    controller

---
+++ src/Controller/CatalogueController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Catalogue;
+use App\Form\CatalogueType;
+use App\Repository\CatalogueRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CatalogueController extends AbstractController
+{
+    #[Route('/catalogues', name: 'catalogues')]
+    public function countries(CatalogueRepository $catalogueRepository): Response
+    {
+        $countries = $catalogueRepository->findAll();
+
+        return $this->render('catalogue/index.html.twig', [
+            'countries' => $countries,
+        ]);
+    }
+
+    #[Route('/catalogue/new', name: 'new_catalogue')]
+    public function newCatalogue(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $catalogue = new Catalogue();
+        $form = $this->createForm(CatalogueType::class, $catalogue);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($catalogue);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('catalogues');
+        }
+
+        return $this->render('catalogue/new_catalogue.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/catalogue/update/{id}', name: 'update_catalogue')]
+    public function updateCatalogue(Catalogue $catalogue, Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        $form = $this->createForm(CityType::class, $catalogue);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($catalogue);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('catalogue', array('catalogueName' => $catalogue->getNom()));
+        }
+
+        return $this->render('catalogue/new_catalogue.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/catalogue/delete/{id}', name: 'delete_catalogue')]
+    public function deleteCatalogue(Catalogue $catalogue, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($catalogue);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('countries');
+    }
+
+    #[Route('/catalogue/{catalogueName}', name: 'catalogue')]
+    public function catalogue(CatalogueRepository $catalogueRepository, string $catalogueName): Response
+    {
+        $catalogue = $catalogueRepository->findOneBy(array('nom' => $catalogueName));
+
+        if (!$catalogue) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('catalogue/detail.html.twig', [
+            'catalogue' => $catalogue,
+        ]);
+    }
+}
---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/produits', name: 'produits')]
+    public function countries(ProduitRepository $produitRepository): Response
+    {
+        $countries = $produitRepository->findAll();
+
+        return $this->render('produit/index.html.twig', [
+            'countries' => $countries,
+        ]);
+    }
+
+    #[Route('/produit/new', name: 'new_produit')]
+    public function newProduit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $produit = new Produit();
+        $form = $this->createForm(ProduitType::class, $produit);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('produits');
+        }
+
+        return $this->render('produit/new_produit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/produit/update/{id}', name: 'update_produit')]
+    public function updateProduit(Produit $produit, Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        $form = $this->createForm(CityType::class, $produit);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('produit', array('produitName' => $produit->getNom()));
+        }
+
+        return $this->render('produit/new_produit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/produit/delete/{id}', name: 'delete_produit')]
+    public function deleteProduit(Produit $produit, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($produit);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('countries');
+    }
+
+    #[Route('/produit/{produitName}', name: 'produit')]
+    public function produit(ProduitRepository $produitRepository, string $produitName): Response
+    {
+        $produit = $produitRepository->findOneBy(array('nom' => $produitName));
+
+        if (!$produit) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('produit/detail.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+}
